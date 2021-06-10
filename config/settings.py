31b75dfeba25e931212267df5c18d0438db61e09
yuya210526 <yuya210526@example.com>
2021-06-11 03:29:27 +0900

    リスト形式追加

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'sample.apps.SampleConfig',
-    'sample2.apps.Sample2Config',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('sample/', include('sample.urls')),
-    path('sample2/', include('sample2.urls')),
     path('admin/', admin.site.urls),
 ]
